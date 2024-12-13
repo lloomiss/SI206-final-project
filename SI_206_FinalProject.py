c3296f3a46fdd06138d68be6049903f2f0b1bb31
Evie Katmanivong <ekatmani@umich.edu>
2024-12-12 20:16:04 -0500

    database is working yasssss

--- SI_206_FinalProject.py
+++ SI_206_FinalProject.py
@@ -7,9 +7,7 @@ import sqlite3
 import json
 import os
 import time
-#headers = {'X-MAL-Client-ID': '6114d00ca681b7701d1e15fe11a4987e'}
-#resp = requests.get('https://api.myanimelist.net/v2/anime/search?status=not_yet_aired&limit=1&offset=0&fields=alternative_titles', headers=headers)
-#print(resp.json())
+
 
 def tomato_extract(tag):
     # get the tags for anime titles, tomatometers, and popcornmeters
@@ -19,10 +17,11 @@ def tomato_extract(tag):
 
     # get the text or return a NULL value if no text
     title = title_tag.get_text(strip=True) if title_tag else "NULL"
+    lower_title = title.lower()
     tomatometer = tomatometer_tag.get_text(strip=True) if tomatometer_tag else "NULL"
     popcornmeter = popcornmeter_tag.get_text(strip=True) if popcornmeter_tag else "NULL"
     
-    return (title, tomatometer, popcornmeter)
+    return (lower_title, tomatometer, popcornmeter)
 
 def get_RT_info(soup) -> list:
     # initialize list of tuples to return
@@ -42,59 +41,68 @@ def get_RT_info(soup) -> list:
     else:
         print("No data found")
     
-    #print(RT_data_list)
     return RT_data_list
+
 
 
 def get_MAL_info(RT_data_list, start, stop) -> list:
     info_list = []
     for anime in RT_data_list[start:stop]:
-        title = anime[0]
-
-        # url here gets "full information from the API" getAnimeFullByld (https://docs.api.jikan.moe/#tag/anime/operation/getAnimeFullById)
-        #url = f'https://api.jikan.moe/v4/anime/{modified_string}/full'
-        url = f'https://api.jikan.moe/v4/anime?q={title}'
+        RT_title = anime[0].lower()
+        # Getting the search results of our title from RT_data_list
+        url = f'https://api.jikan.moe/v4/anime?q={RT_title}'
         response = requests.get(url)
+        
         if response.status_code == 200:
             data = response.json()
-            
             if 'data' in data and len(data['data']) > 0:
-                first_entry = data['data'][0]
-                MAL_ID = first_entry.get('mal_id')
-                
-                if MAL_ID:
-                    full_url = f'https://api.jikan.moe/v4/anime/{MAL_ID}/full'
-                    
-                    #delay in requests
-                    time.sleep(2)
-                    
-                    response2 = requests.get(full_url)
-                    if response2.status_code == 200:
-                        data2 =  response2.json()
-                        fullInfo = []
-                        seasonScore = data2['data'].get('score', 'null') 
-                        numSeasons = -1 # 0 if movie
-                        genres_list = data2['data'].get('genres', [])
-                        genres = [genre.get('name', 'null') for genre in genres_list]
-                        studio_list = data2['data'].get('studios', [])
-                        studio = studio_list[0].get('name', 'null') if studio_list else 'null'
-                        numEpisodes = data2['data'].get('episodes', 'null')
-                        releaseDate = data2['data']['aired'].get('string', 'null') # string formatted as 'Mon XX, XXXX to Mon XX, XXXX'
-                        numReviews = data2['data'].get('scored_by', 'null')
+                fullInfo = []
+                titleMatch = False
+                for entry in data['data']:
+                    MAL_titles = entry.get('titles')
+                    for MAL_title in MAL_titles:
+                        lowerMAL = MAL_title['title'].lower()
+                        if lowerMAL == RT_title:
+                            titleMatch = True
+                            break
+                    if titleMatch:
+                        MAL_ID = entry.get('mal_id')
+                        full_url = f'https://api.jikan.moe/v4/anime/{MAL_ID}/full'
                         
-                        fullInfo.append((title, seasonScore, genres, studio, numEpisodes, releaseDate, numReviews))
+                        # Delay in requests
+                        time.sleep(2)
                         
-                        info_list.append(fullInfo)
-                        
-                    else:
-                        raise Exception(f"Failed to retrieve full data for MAL ID: {MAL_ID}. Response code: {response2.status_code}")
-                else:
-                    raise Exception(f"MAL ID not found for title: {title}")
+                        response2 = requests.get(full_url)
+                        if response2.status_code == 200:
+                            data2 = response2.json()
+                            seasonScore = data2['data'].get('score', 'null')
+                            genres_list = data2['data'].get('genres', [])
+                            genre1 = genres_list[0].get('name', 'null') if genres_list else 'null' #[genre.get('name', 'null') for genre in genres_list]
+                            studio_list = data2['data'].get('studios', [])
+                            studio = studio_list[0].get('name', 'null') if studio_list else 'null'
+                            numEpisodes = data2['data'].get('episodes', 'null')
+                            releaseDate = data2['data']['aired'].get('string', 'null')
+                            numReviews = data2['data'].get('scored_by', 'null')
+                            
+                            fullInfo.append((RT_title, seasonScore, genre1, studio, numEpisodes, releaseDate, numReviews))
+                            info_list.append(fullInfo)
+                        else:
+                            raise Exception(f"Failed to retrieve full data for MAL ID: {MAL_ID}. Response code: {response2.status_code}")
+                        break
+                if not titleMatch:
+                    seasonScore = 'null'
+                    genres = []
+                    studio = 'null'
+                    numEpisodes = 'null'
+                    releaseDate = 'null'
+                    numReviews = 'null'
+                    fullInfo.append((RT_title, seasonScore, genres, studio, numEpisodes, releaseDate, numReviews))
+                    info_list.append(fullInfo)
             else:
-                raise Exception(f"No data found for title: {title}")
+                raise Exception(f"No data found for title: {RT_title}")
         else:
             raise Exception(f"Failed to retrieve data: {response.status_code}")
-    
+
     print(info_list)
     return info_list
 
@@ -115,74 +123,44 @@ def set_up_database(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path + "/" + db_name)
     cur = conn.cursor()
-    return cur, conn     
+    return cur, conn
 
-
-
-'''def create_MAL_table(data, cur, conn):
-    """
-    Parameters
-    -----------------------
-    data: str
-        Stores pokemon.json, written in JSON format
-    
-    cur: 
-        database cursor
-    
-    conn: 
-        database connection
-
-    Returns
-    -----------------------
-    Nothing
-    """
-    # YOUR CODE IMPLEMENTATION HERE
-
+def create_default_table(data, cur, conn):
     cur.execute(
         '''
-        #CREATE TABLE IF NOT EXISTS Pokemon (
-        #anime_id INTEGER PRIMARY KEY,
-        #name TEXT,
-        #score INTEGER,
-        #genre STRING,
-        #studio STRING,
-        #numEpi INTEGER,
-        #releaseDate STRING,
-        #numReviews INTEGER,
-        #)
-'''
-    )
-    for anime in data:
-        name = anime['name']
-        type1 = anime['type'][0]
-        cur.execute("SELECT id FROM Types WHERE type = ?", (type1, ))
-        type1_id = cur.fetchone()[0]
-
-        type2_id = None
-        if len(pokemon['type']) > 1:
-            type2 = pokemon['type'][1]
-            cur.execute("SELECT id FROM Types WHERE type = ?", (type2, ))
-            type2_id = cur.fetchone()[0]
-       #print(pokemon)
-        health_points = pokemon['stats']['hp']
-        speed = pokemon['stats']['speed']
-        attack = pokemon['stats']['attack']
-        special_attack = pokemon['stats']['special-attack']
-        defense = pokemon['stats']['defense']
-        special_defense = pokemon['stats']['special-defense']
-        #print(type1_id, type2_id)
-        cur.execute(
-            '''
-            #INSERT OR IGNORE INTO Pokemon (
-            #pokemon_id, name, type1_id, type2_id, health_points, speed, attack, spl_attack, defense, spl_defense
-            #)
-            #VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-''', (pokemon['id'], name, type1_id, type2_id, health_points, speed, attack, special_attack, defense, special_defense)
+        CREATE TABLE IF NOT EXISTS Anime (
+            anime_id INTEGER PRIMARY KEY,
+            title TEXT,
+            score TEXT,
+            genre TEXT,
+            studio TEXT,
+            numEpi INTEGER,
+            releaseDate TEXT,
+            numReviews INTEGER
         )
+        '''
+    )
     
-    conn.commit()'''
-
+    for entry in data:
+        for anime in entry:  # Ensure that we unpack correctly from the list
+            title = anime[0]
+            score = anime[1]
+            genres = ', '.join(anime[2]) if isinstance(anime[2], list) else anime[2]  # Ensure genre is a string
+            studio = anime[3]
+            numEpisodes = anime[4]
+            releaseDate = anime[5]
+            numReviews = anime[6]
             
+            cur.execute(
+                '''
+                INSERT OR IGNORE INTO Anime (
+                    title, score, genre, studio, numEpi, releaseDate, numReviews
+                )
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+                ''', (title, score, genres, studio, numEpisodes, releaseDate, numReviews)
+            )
+
+    conn.commit()
             
 def main():
     # get soup
@@ -200,10 +178,23 @@ def main():
 
     try:
         anime_details = get_MAL_info(RT_info, 0, 10)
+        cur, conn = set_up_database("anime")
+        create_default_table(anime_details, cur, conn)
         print(anime_details)
     except Exception as e:
-            print(e)
+        print(e)
+
+''' class TestAllMethods(unittest.TestCase):
+    def setUp(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+        self.conn = sqlite3.connect(path + "/" + "pokemon.db") # we don't have our own test database
+        self.cur = self.conn.cursor()
+        self.data = read_data_from_file("pokemon.json") 
 
 
+    def test_get_MAL_Info (self):
+        ## what is our expected output?
+        #self.assertEqual(x, x)
+'''
 if __name__ == "__main__":
     main()
