d0abd31e9ca79fb80149fc30105a4997e223195f
lloomiss <lloomis@umich.edu>
2024-12-16 10:40:39 -0500

    graphs done

--- calculations.py
+++ calculations.py
@@ -35,7 +35,7 @@ def combined_select_n_CSV(conn, cur):
     print(f"Data has been written to {csv_file}")
 
 
-def avg_rating_by_genre(csv_file):
+def avg_rating_by_genre():
     genres = ("Adventure", "Comedy", "Drama", "Fantasy", "Romance", "Sci-Fi", "Slice of Life", "Supernatural")
     score_means = {
         'MAL score': (7.9733, 7.7625, 7.385, 7.69, 7.545, 6.345, 8.17, 8.62),
@@ -64,6 +64,36 @@ def avg_rating_by_genre(csv_file):
     plt.show()
 
 
+def top_5_results():
+    anime = ("Uzumaki", "Pluto", "Solo Leveling", "Delicious in Dungeon", "Frieren: Beyond Journey's End")
+    scores = {
+        'MAL score': (5.89, 8.46, 8.27, 8.61, 9.32),
+        'Tomatometer': (8, 10, 10, 10, 10),
+        'Popcornmeter': (7, 9.5, 8.4, 9.6, 9.5)
+    }
+
+    x = np.arange(len(anime))  # the label locations
+    width = 0.15  # the width of the bars
+    multiplier = 0
+
+    fig, ax = plt.subplots(layout='constrained')
+
+    for attribute, measurement in scores.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, label=attribute)
+        ax.bar_label(rects, padding=3)
+        multiplier += 1
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Rating (out of 10)')
+    ax.set_title('Current Top 10 Anime Ratings')
+    ax.set_xticks(x + width / 2, anime)
+    ax.legend(loc='upper left', ncols=2)
+    ax.set_ylim(0, 11)  # Since scores are out of 10
+
+    plt.show()
+
+
 
 def main():
 
@@ -71,7 +101,9 @@ def main():
     try:
         #cur, conn =  set_up_database('anime.db')
         #combined_select_n_CSV(conn, cur)
-        avg_rating_by_genre("combined_anime_data_output.csv")
+        #avg_rating_by_genre()
+        top_5_results()
+
         
 
     except Exception as e:
