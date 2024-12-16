2e44820d7099fe64ab1ef42087c1aac86ac8ab7a
lloomiss <lloomis@umich.edu>
2024-12-16 09:25:36 -0500

    graphs started

--- calculations.py
+++ calculations.py
@@ -7,8 +7,10 @@ import sqlite3
 import json
 import os
 import time
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 import csv
+
 
 def set_up_database(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -35,6 +37,35 @@ def combined_select_n_CSV(conn, cur):
     print(f"Data has been written to {csv_file}")
 
 
+def avg_rating_by_genre():
+    genres = ("Action", "Adventure", "Comedy", "Drama", "Fantasy", "Romance", "Sci-Fi", "Slice of Life", "Supernatural")
+    score_means = {
+        'MAL score': (1, 2, 3, 4, 5, 6, 7, 8, 9),
+        'RT popcornmeter': (1, 2, 3, 4, 5, 6, 7, 8, 9),
+    }
+
+    x = np.arange(len(genres))  # the label locations
+    width = 0.1  # the width of the bars
+    multiplier = 0
+
+    fig, ax = plt.subplots(layout='constrained')
+
+    for attribute, measurement in score_means.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, label=attribute)
+        ax.bar_label(rects, padding=3)
+        multiplier += 1
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Average Rating (out of 10)')
+    ax.set_title('Average Rating by Platform and Genre')
+    ax.set_xticks(x + width, genres)
+    ax.legend(loc='upper left', ncols=3)
+    ax.set_ylim(0, 250)
+
+    plt.show()
+
+
 
 def main():
 
@@ -42,6 +73,7 @@ def main():
     try:
         cur, conn =  set_up_database('anime.db')
         combined_select_n_CSV(conn, cur)
+        avg_rating_by_genre()
         
 
     except Exception as e:
